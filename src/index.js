9c33a91cb4635200b96131f9e0b3bffba2746832
ERROR\Name <baconcheese113@hotmail.com>
2019-02-21 16:55:00 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+class App extends React.Component {
+  render() {
+    return ''
+  }
+}
+
+let runningSum = 11275;
+const formatter = Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2,
+});
+
+// Change number
+setInterval(() => {
+  const total = document.querySelector('.main-totals h2');
+  runningSum += Math.random();
+  
+  total.innerText = formatter.format(runningSum);
+}, 1000);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
